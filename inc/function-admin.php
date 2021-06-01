ba97223b8ba7696b33a095504c56b2eda06a945b
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-06-01 17:41:05 -0500

    ADD: Submenu Admin Page

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -7,18 +7,39 @@
 */
 add_action( 'admin_menu', 'sunset_add_admin_page' );
 function sunset_add_admin_page() {
+    // Generate Admin Page
     add_menu_page( 
-        __( 'Sunset Theme page', 'sunset' ),
-        __( 'Sunset Theme menu', 'sunset' ),
+        __( 'Sunset Theme Options', 'sunset' ),
+        __( 'Sunset Theme Options', 'sunset' ),
         'manage_options', 
-        'custom-sunset', 
+        'custom_sunset', 
         'sunset_theme_create_page', 
         'dashicons-admin-customizer', 
         110 
     );
+    // Generate Admin Sub Page
+    add_submenu_page(
+        'custom_sunset',
+        'Sunset Theme Options',
+        'General',
+        'manage_options',
+        'custom_sunset',
+        'sunset_theme_create_page'
+
+    );
+    add_submenu_page(
+        'custom_sunset',
+        'Sunset CSS Options',
+        'Custom CSS',
+        'manage_options',
+        'custom_sunset_css',
+        'sunset_theme_settings_page'
+    );
 }
 function sunset_theme_create_page() {
-    echo '<h1>Sunset  Theme Options</h1>';
+    require_once get_template_directory()."/inc/templates/sunset-admin.php";
 }
-
+function sunset_theme_settings_page() {
+    echo '<h1>Sunset  Settings Options</h1>';
+}
 
---
+++ inc/templates/sunset-admin.php
@@ -0,0 +1,4 @@
+<h1>Sunset  Theme Options</h1>
+<h3 class="title">Manage Options</h1>
+<p>Customize the default options on wordpress Appeareance Options</p>
+<?php bloginfo('name'); ?>
